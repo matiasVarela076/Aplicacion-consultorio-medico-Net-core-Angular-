896d20d49d874f772b8e05b8a7c1e20e53323167
Matias Varela <matiasvarela076@gmail.com>
2026-01-25 23:42:52 -0300

    cambios

--- DoctorAppBackend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ DoctorAppBackend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34523f0f13ff63a438aa370ddbf66fecfcd76239")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
